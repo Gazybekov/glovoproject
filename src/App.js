275abf6e3b5b7d09a10c179d0aa1f47f6049c175
Адилет Газыбеков <adiletgazybekov@MacBook-Air-Adilet.local>
2023-05-12 20:28:55 +0600

    save

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddProduct from "./components/Product/AddProduct";
 import Katalog from "./components/Katalog/Katalog";
 import Nav from "./components/Nav/Nav";
 import PartnersMain from "./components/Partners_page/PartnersMain";
@@ -7,12 +8,12 @@ import PartnersMain from "./components/Partners_page/PartnersMain";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Nav />} />
-          <Route path="/kata" element={<Katalog />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/products" element={<AddProduct />} />
+        <Route path="/kfc" element={<PartnersMain />} />
+        <Route path="/" element={<Nav />} />
+        <Route path="/kata" element={<Katalog />} />
+      </Routes>
     </>
   );
 };
