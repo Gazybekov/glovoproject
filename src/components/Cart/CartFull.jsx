3c5207f6acb8668e65877c90f5afe18f876a6903
Neilom <nil.porvani@gmail.com>
2023-05-13 17:49:13 +0600

    try

--- src/components/Cart/CartFull.jsx
+++ src/components/Cart/CartFull.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import "../Partners_page/style/Header.css";
 import { useCart } from "../../context/CartContextProvider";
-import pluss from "../Partners_page/img/header_img/plus-new.svg";
 import minus from "../Partners_page/img/header_img/minus.svg";
+import { useProducts } from "../../context/ProductContextProvider";
+import { Box, Grid, Pagination } from "@mui/material";
+import CardItem from "../Partners_page/CardItem";
+import "../Partners_page/style/Header.css";
+import pluss from "../Partners_page/img/header_img/plus-new.svg";
 import { calcTotalPrice } from "../../helpers/function";
 
-export default function CartFull() {
+export default function CartFull({ item }) {
   const {
     getCart,
     cart,
@@ -47,33 +51,64 @@ export default function CartFull() {
     return totalCount;
   };
 
+  const { products, getProducts } = useProducts();
+  const [page, setPage] = useState(1);
+
+  const itemsPerPage = 6;
+  const count = Math.ceil(products.length / itemsPerPage);
+
+  const handleChange = (e, p) => {
+    setPage(p);
+  };
+  function currentData() {
+    const begin = (page - 1) * itemsPerPage;
+    const end = begin + itemsPerPage;
+    return products.slice(begin, end);
+  }
+
   return (
-    <div className="cart-section_full">
-      <div className="cart-header">
-        <h2>Ваш заказ</h2>
-      </div>
-      <div className="box-fullcart">
-        {cart.products.map((item) => (
-          <div key={item.item.id}>
-            <div className="cart-textbox">
-              <p>{item.count}X</p>
-              <p>{item.item.name}</p>
-              <span>{item.item.price}</span>
-            </div>
-            <div className="btn_cart">
-              <button onClick={() => handleDecrease(item)}>
-                <img src={minus} alt="" />
-              </button>
-              <button onClick={() => handleIncrease(item)}>
-                <img src={pluss} alt="" />
-              </button>
-            </div>
+    <div>
+      <Grid item md={9}>
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            my: "2rem",
+            justifyContent: "space-evenly",
+          }}
+        >
+          {currentData().map((item) => (
+            <CardItem key={item.id} item={item} />
+          ))}
+        </Box>
+        <Pagination
+          count={count}
+          page={page}
+          onChange={handleChange}
+          variant="outlined"
+          color="secondary"
+        />
+      </Grid>
+      <div className="cart-section_full">
+        <div className="cart-header">
+          <h2>Ваш заказ</h2>
+        </div>
+        <div className="box-fullcart">
+          <div className="cart-textbox">
+            <p>1x</p>
+            <p>name</p>
+            <span>price</span>
           </div>
-        ))}
-        <button className="buybtn">
-          Заказать продукты ({getTotalProductCount(cart.products)}) за&nbsp;
-          {calcTotalPrice(cart.products)} KGS
-        </button>
+          <div className="btn_cart">
+            <button onClick={() => addProductToCart(item)}>
+              <img src={minus} alt="" />
+            </button>
+            <button onClick={() => addProductToCart(item)}>
+              <img src={pluss} alt="" />
+            </button>
+          </div>
+          <button>Заказать продукты (3) за цена</button>
+        </div>
       </div>
     </div>
   );
