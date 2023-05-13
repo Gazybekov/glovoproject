20f8f7165a2aa91ed0f3fbc3c1d2e70c427c1a3e
Адилет Газыбеков <adiletgazybekov@MacBook-Air-Adilet.local>
2023-05-13 15:51:06 +0600

    save15

--- src/components/Partners_page/CardItem.jsx
+++ src/components/Partners_page/CardItem.jsx
@@ -1,9 +1,14 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useCart } from "../../context/CartContextProvider";
+import { useProducts } from "../../context/ProductContextProvider";
 import plus from "./img/header_img/plus-new.svg";
 import "./style/Header.css";
 
 const CardItem = ({ item }) => {
+  const { deleteProduct } = useProducts();
   const { addProductToCart } = useCart();
+  const navigate = useNavigate();
   return (
     <div className="main_wrapper">
       <div className="card_box">
@@ -15,6 +20,8 @@ const CardItem = ({ item }) => {
       </div>
       <div className="price">
         <span>{item.price},00 KGS</span>
+        <button onClick={() => deleteProduct(item.id)}>delete</button>
+        <button onClick={() => navigate(`/edit/${item.id}`)}>edit</button>
         <button onClick={() => addProductToCart(item)}>
           <img src={plus} alt="" />
         </button>
