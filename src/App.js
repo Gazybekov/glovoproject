dc69c3a70277a68942962356fe8a902fc017649f
Neilom <nil.porvani@gmail.com>
2023-05-17 14:19:47 +0600

    please

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import PartnersMain from "./components/Partners_page/PartnersMain";
 import EditCardPage from "./components/Product/EditCardPage";
 import CardForm from "./components/Product/CardForm";
 import Register from "./components/Register/Register";
-import Payment from "./components/Payment/Payment";
 
 
 const App = () => {
@@ -22,7 +21,6 @@ const App = () => {
         <Route path="/edit/:id" element={<EditCardPage />} />
         <Route path="/form" element={<CardForm />} />
         <Route path="/registr" element={<Register/>} />
-        <Route path="/pay" element={<Payment></Payment>} />
       </Routes>
     </>
   );
